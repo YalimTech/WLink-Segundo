ac6be3857c7e11428e1e398224cf890082c7d1de
YalimTech <30573421+YalimTech@users.noreply.github.com>
2025-08-18 01:09:49 -0400

    feat: Initial commit of WLink-Segundo project

--- prisma/migrations/20250818050558_add_ghl_user_id_to_schema/migration.sql
+++ prisma/migrations/20250818050558_add_ghl_user_id_to_schema/migration.sql
@@ -2,6 +2,23 @@
 CREATE TYPE "InstanceState" AS ENUM ('notAuthorized', 'qr_code', 'authorized', 'yellowCard', 'blocked', 'starting');
 
 -- CreateTable
+CREATE TABLE "users" (
+    "locationId" TEXT NOT NULL,
+    "companyId" TEXT,
+    "ghlUserId" TEXT,
+    "firstName" TEXT,
+    "lastName" TEXT,
+    "email" TEXT,
+    "accessToken" TEXT NOT NULL,
+    "refreshToken" TEXT NOT NULL,
+    "tokenExpiresAt" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("locationId")
+);
+
+-- CreateTable
 CREATE TABLE "instances" (
     "id" BIGSERIAL NOT NULL,
     "instanceName" TEXT NOT NULL,
@@ -17,34 +34,17 @@ CREATE TABLE "instances" (
     CONSTRAINT "instances_pkey" PRIMARY KEY ("id")
 );
 
--- CreateTable
-CREATE TABLE "users" (
-    "locationId" TEXT NOT NULL,
-    "companyId" TEXT,
-    "firstName" TEXT,
-    "lastName" TEXT,
-    "email" TEXT,
-    "accessToken" TEXT NOT NULL,
-    "refreshToken" TEXT NOT NULL,
-    "tokenExpiresAt" TIMESTAMP(3),
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "users_pkey" PRIMARY KEY ("locationId")
-);
+-- CreateIndex
+CREATE UNIQUE INDEX "users_locationId_key" ON "users"("locationId");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "instances_instanceId_key" ON "instances"("instanceId" ASC);
+CREATE UNIQUE INDEX "instances_instanceName_key" ON "instances"("instanceName");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "instances_instanceName_key" ON "instances"("instanceName" ASC);
+CREATE UNIQUE INDEX "instances_instanceId_key" ON "instances"("instanceId");
 
 -- CreateIndex
-CREATE INDEX "instances_locationId_idx" ON "instances"("locationId" ASC);
-
--- CreateIndex
-CREATE UNIQUE INDEX "users_locationId_key" ON "users"("locationId" ASC);
+CREATE INDEX "instances_locationId_idx" ON "instances"("locationId");
 
 -- AddForeignKey
 ALTER TABLE "instances" ADD CONSTRAINT "instances_locationId_fkey" FOREIGN KEY ("locationId") REFERENCES "users"("locationId") ON DELETE CASCADE ON UPDATE CASCADE;
-
